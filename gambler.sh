a976e0e0c63f3add0ceacfd1a88007c355931c0e
riyazthk <er.riyaz2507@gmail.com>
2020-07-06 19:34:13 +0530

    ADDED:To calculate a win or lose for a per bet

--- gambler.sh
+++ gambler.sh
@@ -3,3 +3,15 @@
 #declare variables
 stack=100
 bet=1
+
+result=$(( RANDOM%2 ))
+if [[ $result -eq 0 ]]
+then
+  stack=$(( $stack-$bet ))
+  echo "win"
+  echo $stack
+else
+  stack=$(( $stack-$bet ))
+  echo "lose"
+  echo $stack
+fi
