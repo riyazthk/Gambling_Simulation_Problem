a3ab3044eb6b2a64e8f4f757dca65555afac730e
riyazthk <er.riyaz2507@gmail.com>
2020-07-06 19:52:54 +0530

    ADDED:To calculate a stack upto 50%

--- gambler.sh
+++ gambler.sh
@@ -4,14 +4,18 @@
 stack=100
 bet=1
 
+#To calculate a stack upto 50%
+#using random function to calculate a result
+#if result is 0 stack is increment with bet
+#if result is 1 stack is decrement with bet
+while [[ $stack -gt 50 && $stack -lt 150 ]]
+do
 result=$(( RANDOM%2 ))
 if [[ $result -eq 0 ]]
 then
-  stack=$(( $stack-$bet ))
-  echo "win"
-  echo $stack
+  stack=$(( $stack+$bet ))
 else
   stack=$(( $stack-$bet ))
-  echo "lose"
-  echo $stack
 fi
+done
+echo $stack
