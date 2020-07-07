778159a5d565e30791a29c77bcbf9e6ff56be526
riyazthk <er.riyaz2507@gmail.com>
2020-07-07 20:07:06 +0530

    ADDED:To calculate each month no of days won and loss finally calculate how much they loss

--- gambler.sh
+++ gambler.sh
@@ -1,48 +1,66 @@
 #! /bin/bash -x
 
 #declare variables
-stack=100
+stake=100
 bet=1
-totalamount=0
+totalAmount=0
 win=0
-totaldays=20
+totalDays=20
+day=1
+s=0
+count=1
 
+declare -a dayAmount
 #calculate a 50% of a stack for min and max
-percent=$(( $stack *50 / 100))
-min=$(( $stack-$percent ))
-max=$(( $stack+$percent ))
+percent=$(( $stake * 50 / 100 ))
+min=$(( $stake-$percent ))
+max=$(( $stake+$percent ))
 
 #if result is 0 stack is increment with bet
 #if result is 1 stack is decrement with bet
-function CalculateStack(){
+function calculateStake(){
     local result=$1
 
     if [[ $result -eq $win ]]
     then
-        stack=$(( $stack+$bet ))
+        stake=$(( $stake+$bet ))
     else
-        stack=$(( $stack-$bet ))
+        stake=$(( $stake-$bet ))
     fi
-    echo $(( $stack ))
+    echo $(( $stake ))
 }
 
 #To calculate a stack upto 50%
 #using random function to calculate a result
-function PerDayStack(){
-   while [[ $stack -gt $min && $stack -lt $max ]]
+function perDayStake(){
+   while [[ $stake -gt 90 && $stake -lt 110 ]]
    do
       result=$(( RANDOM%2 ))
-      stack=$(CalculateStack $result)
+      stake=$(calculateStake $result)
    done
-   echo $(( $stack ))
+   s=$(( $stake ))
+   echo $(( $stake ))
 }
 
 #To calculate a perday stack upto 20 days
 #Print a total amount
-while [[ $day -le $totaldays ]]
-do
-perdaystack=$(PerDayStack)
-totalamount=$(( $perdaystack + $totalamount ))
-day=$(( $day + 1 ))
-done
-echo $totalamount
+function totalStake(){
+   while [[ $day -le 5 ]]
+   do
+      perStake=$(perDayStake)
+      totalAmount=$(( $perStake + $totalAmount ))
+      dayAmount[ (day) ]=$(( $perStake ))
+      day=$(( $day + 1 ))
+   done
+   for i in  "${!dayAmount[@]}"
+   do
+   echo "$i=>${dayAmount[$i]}"
+   if [[ ${dayAmount[i]} -eq 90 ]]
+   then
+   loss=$(( $loss + ($stake-${dayAmount[i]}) ))
+   fi
+   done
+   echo $loss
+}
+totalStake
+
