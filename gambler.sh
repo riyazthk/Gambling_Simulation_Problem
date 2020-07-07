2f9137223887e9934e10edc0052c3f7ea24366ff
riyazthk <er.riyaz2507@gmail.com>
2020-07-07 23:08:07 +0530

    [Riyaz]ADDED:To find a maximum won and minimum won in a month

--- gambler.sh
+++ gambler.sh
@@ -5,16 +5,19 @@ stake=100
 bet=1
 totalAmount=0
 win=0
-totalDays=20
+totalDays=3
 day=1
-s=0
-count=1
+perStake=0
 
 declare -a dayAmount
+
 #calculate a 50% of a stack for min and max
-percent=$(( $stake * 50 / 100 ))
-min=$(( $stake-$percent ))
-max=$(( $stake+$percent ))
+function calculatePercent(){
+
+       local stake=$1
+       percent=$(( $stake * 50 / 100 ))
+       echo $((percent))
+}
 
 #if result is 0 stack is increment with bet
 #if result is 1 stack is decrement with bet
@@ -33,34 +36,44 @@ function calculateStake(){
 #To calculate a stack upto 50%
 #using random function to calculate a result
 function perDayStake(){
-   while [[ $stake -gt 90 && $stake -lt 110 ]]
+   local min=$1
+   local max=$2
+
+   while [[ $stake -gt $min && $stake -lt $max ]]
    do
       result=$(( RANDOM%2 ))
       stake=$(calculateStake $result)
    done
-   s=$(( $stake ))
-   echo $(( $stake ))
+    echo $(( $stake ))
 }
 
 #To calculate a perday stack upto 20 days
 #Print a total amount
 function totalStake(){
-   while [[ $day -le 5 ]]
+   while [[ $day -le $totalDays ]]
    do
-      perStake=$(perDayStake)
-      totalAmount=$(( $perStake + $totalAmount ))
+      totalAmount=$(( $stake + $perStake ))
+      percent=$(calculatePercent $totalAmount)
+      min=$(( $stake-$percent ))
+      max=$(( $stake+$percent ))
+      perStake=$(perDayStake $min $max)
       dayAmount[ (day) ]=$(( $perStake ))
       day=$(( $day + 1 ))
    done
    for i in  "${!dayAmount[@]}"
    do
    echo "$i=>${dayAmount[$i]}"
-   if [[ ${dayAmount[i]} -eq 90 ]]
-   then
-   loss=$(( $loss + ($stake-${dayAmount[i]}) ))
-   fi
    done
-   echo $loss
 }
 totalStake
-
+#sort a dayAmount and store in a another array
+#and find out maximum won and minimum won
+dayAmount2=($(echo ${dayAmount[@]}| tr " " "\n" | sort -n))
+length=${#dayAmount2[@]}
+for (( i=1;i<=$length;i++ ))
+do
+if [[ ${dayAmount2[0]} -eq ${dayAmount[i]} || ${dayAmount2[length]} -eq ${dayAmount[i]} ]]
+then
+echo "$i=>${dayAmount[$i]}"
+fi
+done
