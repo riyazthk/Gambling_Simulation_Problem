7503857f1663d3e0773cd4b82b44b356f8886f69
riyazthk <er.riyaz2507@gmail.com>
2020-07-07 12:02:52 +0530

    UPDATE:To change and refactor a code

--- gambler.sh
+++ gambler.sh
@@ -1,18 +1,23 @@
 #! /bin/bash -x
 
 #declare variables
-
 stack=100
 bet=1
 totalamount=0
+win=0
+totaldays=20
+
+#calculate a 50% of a stack for min and max
+percent=$(( $stack *50 / 100))
+min=$(( $percent-$stack ))
+max=$(( $percent+$stack ))
 
 #if result is 0 stack is increment with bet
 #if result is 1 stack is decrement with bet
-
-function calculatestack(){
+function CalculateStack(){
     local result=$1
 
-    if [[ $result -eq 0 ]]
+    if [[ $result -eq $win ]]
     then
         stack=$(( $stack+$bet ))
     else
@@ -23,22 +28,20 @@ function calculatestack(){
 
 #To calculate a stack upto 50%
 #using random function to calculate a result
-
-function perdaystack(){
-   while [[ $stack -gt 50 && $stack -lt 150 ]]
+function PerDayStack(){
+   while [[ $stack -gt $min && $stack -lt $max ]]
    do
       result=$(( RANDOM%2 ))
-      stack=$(calculatestack $result)
+      stack=$(CalculateStack $result)
    done
    echo $(( $stack ))
 }
 
 #To calculate a perday stack upto 20 days
 #Print a total amount
-
-while [[ $day -le 20 ]]
+while [[ $day -le $totaldays ]]
 do
-perdaystack=$(perdaystack)
+perdaystack=$(PerDayStack)
 totalamount=$(( $perdaystack + $totalamount ))
 day=$(( $day + 1 ))
 done
